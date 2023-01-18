f4593db7ba7366481559d10343ea33cd097f072a
RaphaelLestruhaut <61656476+AbigaelAnderson@users.noreply.github.com>
2023-01-18 17:10:30 +0100

    menus level

--- Assets/Scripts/managerMenus.cs
+++ Assets/Scripts/managerMenus.cs
@@ -6,34 +6,52 @@ using UnityEngine.UI;
 public class managerMenus : MonoBehaviour
 {
     #region ui
+    #region gameObject
     public GameObject mainMenus;
     public GameObject optionsMenus;
     public GameObject leaderBoardMenus;
+    public GameObject levelMenus;
+    #endregion
 
+    #region menus principal
     public Button buttonQuit;
     public Button buttonOptions;
     public Button buttonLeaderBoard;
+    public Button buttonLevel;
+    #endregion
 
+    #region options
     public Button buttonRetourOptions;
+    #endregion
 
+    #region leaderBoard
     public Button buttonLeaderBoardBack;
+    #endregion
+
+    #region level
+    public Button buttonRetourLevel;
+    #endregion
     #endregion
     // Start is called before the first frame update
     void Start()
     {
         optionsMenus.SetActive(false);
         leaderBoardMenus.SetActive(false);
+        levelMenus.SetActive(false);
         buttonQuit.onClick.AddListener(quit);
         buttonOptions.onClick.AddListener(options);
         buttonRetourOptions.onClick.AddListener(retourOptions);
         buttonLeaderBoardBack.onClick.AddListener(retourLeaderBoard);
         buttonLeaderBoard.onClick.AddListener(leaderBoards);
+        buttonLevel.onClick.AddListener(startGame);
+        buttonRetourLevel.onClick.AddListener(retourLevel);
     }
 
     #region main menus
     private void startGame()
     {
-
+        levelMenus.SetActive(true);
+        mainMenus.SetActive(false);
     }
 
     private void leaderBoards()
@@ -65,6 +83,14 @@ public class managerMenus : MonoBehaviour
     private void retourLeaderBoard()
     {
         leaderBoardMenus.SetActive(false);
+        mainMenus.SetActive(true);
+    }
+    #endregion
+
+    #region level
+    private void retourLevel()
+    {
+        levelMenus.SetActive(false);
         mainMenus.SetActive(true);
     }
     #endregion
