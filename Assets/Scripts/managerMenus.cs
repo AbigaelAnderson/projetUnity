92dddde41e320da1306a1a7f87ced145b4b5e4e4
RaphaelLestruhaut <61656476+AbigaelAnderson@users.noreply.github.com>
2023-01-18 16:57:26 +0100

    bouton leaderboard

--- Assets/Scripts/managerMenus.cs
+++ Assets/Scripts/managerMenus.cs
@@ -8,30 +8,38 @@ public class managerMenus : MonoBehaviour
     #region ui
     public GameObject mainMenus;
     public GameObject optionsMenus;
+    public GameObject leaderBoardMenus;
 
     public Button buttonQuit;
     public Button buttonOptions;
+    public Button buttonLeaderBoard;
 
     public Button buttonRetourOptions;
+
+    public Button buttonLeaderBoardBack;
     #endregion
     // Start is called before the first frame update
     void Start()
     {
         optionsMenus.SetActive(false);
+        leaderBoardMenus.SetActive(false);
         buttonQuit.onClick.AddListener(quit);
         buttonOptions.onClick.AddListener(options);
         buttonRetourOptions.onClick.AddListener(retourOptions);
+        buttonLeaderBoardBack.onClick.AddListener(retourLeaderBoard);
+        buttonLeaderBoard.onClick.AddListener(leaderBoards);
     }
 
     #region main menus
-    private void start()
+    private void startGame()
     {
 
     }
 
     private void leaderBoards()
     {
-
+        leaderBoardMenus.SetActive(true);
+        mainMenus.SetActive(false);
     }
     private void options()
     {
@@ -45,11 +53,21 @@ public class managerMenus : MonoBehaviour
     }
     #endregion
 
+    #region options
     private void retourOptions()
     {
         optionsMenus.SetActive(false);
         mainMenus.SetActive(true);
     }
+    #endregion
+
+    #region leaderBoard
+    private void retourLeaderBoard()
+    {
+        leaderBoardMenus.SetActive(false);
+        mainMenus.SetActive(true);
+    }
+    #endregion
 
     // Update is called once per frame
     void Update()
