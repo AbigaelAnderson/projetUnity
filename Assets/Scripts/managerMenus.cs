18c55091f52e9bfa4444c0a7d7fd2be715b12bc7
RaphaelLestruhaut <61656476+AbigaelAnderson@users.noreply.github.com>
2023-01-18 16:49:07 +0100

    menus options

--- Assets/Scripts/managerMenus.cs
+++ Assets/Scripts/managerMenus.cs
@@ -5,14 +5,25 @@ using UnityEngine.UI;
 
 public class managerMenus : MonoBehaviour
 {
+    #region ui
     public GameObject mainMenus;
+    public GameObject optionsMenus;
+
     public Button buttonQuit;
+    public Button buttonOptions;
+
+    public Button buttonRetourOptions;
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
+        optionsMenus.SetActive(false);
         buttonQuit.onClick.AddListener(quit);
+        buttonOptions.onClick.AddListener(options);
+        buttonRetourOptions.onClick.AddListener(retourOptions);
     }
 
+    #region main menus
     private void start()
     {
 
@@ -24,12 +35,20 @@ public class managerMenus : MonoBehaviour
     }
     private void options()
     {
-
+        optionsMenus.SetActive(true);
+        mainMenus.SetActive(false);
     }
 
     private void quit()
     {
         Application.Quit();
+    }
+    #endregion
+
+    private void retourOptions()
+    {
+        optionsMenus.SetActive(false);
+        mainMenus.SetActive(true);
     }
 
     // Update is called once per frame
