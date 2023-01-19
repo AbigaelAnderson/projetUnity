30a5dfda6cf820969df8335ad8280fb245aa1f18
Antoine LE NAGARD <alenagard57@gmail.com>
2023-01-19 11:54:22 +0100

    New Prefabs Ecran Win et zone Arrivee

---
+++ Assets/Scripts/PersoGoOutView.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersoGoOutView : MonoBehaviour
+{
+
+    public void OnBecameInvisible()
+    {
+        Debug.Log("sorti de vue");
+    }
+
+
+}
