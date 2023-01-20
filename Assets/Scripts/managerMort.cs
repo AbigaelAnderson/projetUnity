e2c7cdaf8011f3fcdc49e3fa439fb15f0d463345
RaphaelLestruhaut <61656476+AbigaelAnderson@users.noreply.github.com>
2023-01-20 09:37:34 +0100

    mise à jour scene

--- Assets/Scripts/managerMort.cs
+++ Assets/Scripts/managerMort.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class managerMort : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Button buttonMenus;
+    public Button buttonReTry;
     void Start()
     {
-        
+        buttonMenus.onClick.AddListener(menus);
+        buttonReTry.onClick.AddListener(reTry);
+    }
+
+    private void menus()
+    {
+        SceneManager.LoadScene("menus");
+    }
+
+    private void reTry()
+    {
+        SceneManager.LoadScene("SceneNiveauFacile");
     }
 
     // Update is called once per frame
